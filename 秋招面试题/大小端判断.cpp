9951b5dc3dc1e06b93fac736a006459b15d04086
KamSss <707855527@qq.com>
2019-10-11 22:54:49 +0800

    大小端判断

---
+++ 秋招面试题/大小端判断.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int i = 1;
+	char* a = (char*)&i;
+	if (*b == 1) {
+		cout << "Ð¡¶Ë" << endl;
+	}
+	else {
+		cout << "´ó¶Ë" << endl;
+	}
+	return 0;
+}
